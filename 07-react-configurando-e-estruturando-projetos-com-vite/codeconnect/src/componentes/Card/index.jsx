6df156ae46c694e122575e2a1dbc9564ee109cbc
Alberto Lima <alberto.btlima@outlook.com>
2025-04-05 19:29:31 -0300

    adicionado conexão com api

--- 07-react-configurando-e-estruturando-projetos-com-vite/codeconnect/src/componentes/Card/index.jsx
+++ 07-react-configurando-e-estruturando-projetos-com-vite/codeconnect/src/componentes/Card/index.jsx
@@ -1,21 +1,28 @@
 import "./styles.css";
-import Capa from "./assets/capa.png";
 import Code from "./assets/code.svg";
 import Share from "./assets/share.svg";
 import Chat from "./assets/chat.svg";
-import Icone from "./assets/icone.png";
 
-export default function Card() {
+export default function Card({
+  id,
+  imagemUrl,
+  titulo,
+  resumo,
+  linhasDeCodigo,
+  compartilhamentos,
+  comentarios,
+  usuario,
+}) {
   return (
     <article className="card">
       <div className="card__imagem">
-        <img src={Capa} alt="Imagem do post" />
+        <img src={imagemUrl} alt="Imagem do post" />
       </div>
 
       <div className="card__conteudo">
         <div className="conteudo__texto">
-          <h3>Título do post</h3>
-          <p>Resumo do post</p>
+          <h3>{titulo}</h3>
+          <p>{resumo}</p>
         </div>
       </div>
 
@@ -23,21 +30,21 @@ export default function Card() {
         <ul>
           <li>
             <img src={Code} alt="códigos" />
-            100
+            {linhasDeCodigo}
           </li>
           <li>
             <img src={Share} alt="compartilhamentos" />
-            12
+            {compartilhamentos}
           </li>
           <li>
             <img src={Chat} alt="comentários" />
-            10
+            {comentarios}
           </li>
         </ul>
 
         <div className="rodape__usuario">
-          <img src={Icone} alt="imagem do usuário" />
-          @Chaves
+          <img src={usuario.imagem} alt="imagem do usuário" />
+          {usuario.nome}
         </div>
       </div>
     </article>
